2adbed04f1c87cfbbec46b1f9994951e07d78023
EEEBuddy <eeebuddyfyp@gmail.com>
2020-01-28 04:55:51 +0800

    Onclick senior buddy info page

--- app/src/main/java/com/example/EEEBuddy/SeniorBuddyModel.java
+++ app/src/main/java/com/example/EEEBuddy/SeniorBuddyModel.java
@@ -6,21 +6,25 @@ public class SeniorBuddyModel {
     private String course;
     private String year;
     private String profileImageUrl;
-    private String experience;
+    private String dateJoin;
     private String selfIntro;
+    private String gender;
+    private String hall;
 
     public SeniorBuddyModel(){
 
     }
 
-    public SeniorBuddyModel(String name, String email, String course, String year, String profileImageUrl, String experience, String selfIntro) {
+    public SeniorBuddyModel(String name, String email, String course, String year, String profileImageUrl, String dateJoin, String selfIntro, String hall, String gender) {
         this.name = name;
         this.email = email;
         this.course = course;
         this.year = year;
         this.profileImageUrl = profileImageUrl;
-        this.experience = experience;
+        this.dateJoin = dateJoin;
         this.selfIntro = selfIntro;
+        this.hall = hall;
+        this.gender = gender;
     }
 
     public void setName(String name) {
@@ -43,8 +47,8 @@ public class SeniorBuddyModel {
         this.profileImageUrl = profileImageUrl;
     }
 
-    public void setExperience(String experience) {
-        this.experience = experience;
+    public void setDateJoin(String experience) {
+        this.dateJoin = dateJoin;
     }
 
     public void setSelfIntro(String selfIntro) {
@@ -71,11 +75,27 @@ public class SeniorBuddyModel {
         return profileImageUrl;
     }
 
-    public String getExperience() {
-        return experience;
+    public String getDateJoin() {
+        return dateJoin;
     }
 
     public String getSelfIntro() {
         return selfIntro;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHall() {
+        return hall;
+    }
+
+    public void setHall(String hall) {
+        this.hall = hall;
     }
 }
